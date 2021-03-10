73c2408c4162d0fb8032a7381784a8e76b69354c
tambegaurav <gauravkishortambe@gmail.com>
2021-03-10 18:06:33 +0530

    add extra people modal

---
+++ index.js
@@ -0,0 +1,42 @@
+const changeNumbers = () => {
+  var text = document.querySelector(".no-of-people");
+
+  console.log(text.textContent);
+
+  var modal = document.querySelector(".add-people-modal");
+  modal.style.display = "block";
+};
+
+const add = () => {
+  var noOfAdults = document.getElementById("noOfAdults");
+  var num = Number(noOfAdults.textContent);
+  console.log(num + 1);
+
+  if (num < 8) {
+    num = num + 1;
+  }
+  noOfAdults.textContent = num;
+
+  var text = document.querySelector(".no-of-people");
+  text.textContent = `1 room, ${num} travellers`;
+};
+
+const subtract = () => {
+  var noOfAdults = document.getElementById("noOfAdults");
+  var num = Number(noOfAdults.textContent);
+  console.log(num + 1);
+  if (num > 1) {
+    num = num - 1;
+  }
+  noOfAdults.textContent = num;
+
+  var text = document.querySelector(".no-of-people");
+  text.textContent = `1 room, ${num} travellers`;
+};
+
+var closeAddModal = document.querySelector("#closeAddModal");
+
+var closeAddMod = () => {
+  document.querySelector(".add-people-modal").style.display = "none";
+};
+closeAddModal.addEventListener("click", closeAddMod);
