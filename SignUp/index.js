f43264c568273924cc779dbd01afbac24ef600f5
tambegaurav <gauravkishortambe@gmail.com>
2021-03-13 01:13:51 +0530

    change review box style and minor fixes

--- SignUp/index.js
+++ SignUp/index.js
@@ -1,25 +1,23 @@
 function signup(el) {
-    el.preventDefault()
-    var isPresent = false;
-    var email = document.getElementById("email").value
-    var pwd = document.getElementById("pwd").value
-    
-    
-    if(email=="" || pwd==""){
-        alert("Input field can't be blank")
+  el.preventDefault();
+  var isPresent = false;
+  var email = document.getElementById("email").value;
+  var pwd = document.getElementById("pwd").value;
+
+  if (email == "" || pwd == "") {
+    alert("Input field can't be blank");
+  } else {
+    for (key in localStorage) {
+      if (key == email) {
+        isPresent = true;
+        alert("User already exists");
+        window.location.assign("../SignIn/index.html");
+      }
     }
-    else{
-        for (key in localStorage) {
-            if (key == email) {
-                isPresent = true;
-                alert("User already exists")
-                window.location.assign("http://127.0.0.1:5500/expedia-clone/SignIn/index.html")
-            }
-        }
-        if (!isPresent) {
-            localStorage.setItem(email, pwd)
-            window.location.assign("http://127.0.0.1:5500/expedia-clone/SignIn/index.html")
-        }
+    if (!isPresent) {
+      localStorage.setItem(email, pwd);
+      window.location.assign("../SignIn/index.html");
     }
+  }
 }
-document.getElementById("submit").addEventListener("click", signup)
+document.getElementById("submit").addEventListener("click", signup);
