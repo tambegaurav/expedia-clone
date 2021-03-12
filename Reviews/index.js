c16f38fdf11cbc4cc74c3007315302260c5bef09
Kamal <kamaldro97@gmail.com>
2021-03-12 11:32:41 +0530

    reviews page workable in all device

--- Reviews/index.js
+++ Reviews/index.js
@@ -3,6 +3,102 @@ var newdata =
 {
     review_type :'5/5 Excellent',
     verified_traveller:'Verified traveler',
+    travel_type:'Couples',
+    feedback:'Liked',
+    date: new Date(),
+    feedback_category:'Cleanliness, staff & service, amenities, property conditions & facilities',
+    feedback_comment:'Cordial staff. Place is neat and clean.Wish cofffee is available 24 hours',
+    spent_time:'Stayed 1 night in Feb 2021',
+    like_counts:0
+}
+       old.push(newdata);
+
+var newdata = 
+{
+    review_type :'5/5 Excellent',
+    verified_traveller:'Verified traveler',
+    travel_type:'Solo travelers',
+    feedback:'Liked',
+    date: new Date(),
+    feedback_category:'Cleanliness, staff & service, amenities, property conditions & facilities',
+    feedback_comment:'Cordial staff. Place is neat and clean.Wish cofffee is available 24 hours',
+    spent_time:'Stayed 1 night in Feb 2021',
+    like_counts:0
+}
+       old.push(newdata);
+
+
+
+var newdata = 
+{
+    review_type :'5/5 Excellent',
+    verified_traveller:'Verified traveler',
+    travel_type:'Business travelers',
+    feedback:'Liked',
+    date: new Date(),
+    feedback_category:'Cleanliness, staff & service, amenities, property conditions & facilities',
+    feedback_comment:'Cordial staff. Place is neat and clean.Wish cofffee is available 24 hours',
+    spent_time:'Stayed 1 night in Feb 2021',
+    like_counts:0
+}
+       old.push(newdata);
+       
+
+var newdata = 
+{
+    review_type :'5/5 Excellent',
+    verified_traveller:'Verified traveler',
+    travel_type:'Families',
+    feedback:'Liked',
+    date: new Date(),
+    feedback_category:'Cleanliness, staff & service, amenities, property conditions & facilities',
+    feedback_comment:'Cordial staff. Place is neat and clean.Wish cofffee is available 24 hours',
+    spent_time:'Stayed 1 night in Feb 2021',
+    like_counts:0
+}
+       old.push(newdata);
+
+
+
+
+var newdata = 
+{
+    review_type :'5/5 Excellent',
+    verified_traveller:'Verified traveler',
+    travel_type:'Families with small children',
+    feedback:'Liked',
+    date: new Date(),
+    feedback_category:'Cleanliness, staff & service, amenities, property conditions & facilities',
+    feedback_comment:'Cordial staff. Place is neat and clean.Wish cofffee is available 24 hours',
+    spent_time:'Stayed 1 night in Feb 2021',
+    like_counts:0
+}
+       old.push(newdata);
+
+
+
+
+
+var newdata = 
+{
+    review_type :'5/5 Excellent',
+    verified_traveller:'Verified traveler',
+    travel_type:'Groups',
+    feedback:'Liked',
+    date: new Date(),
+    feedback_category:'Cleanliness, staff & service, amenities, property conditions & facilities',
+    feedback_comment:'Cordial staff. Place is neat and clean.Wish cofffee is available 24 hours',
+    spent_time:'Stayed 1 night in Feb 2021',
+    like_counts:0
+}
+       old.push(newdata);
+
+
+
+var newdata = 
+{
+    review_type :'5/5 Excellent',
+    verified_traveller:'Verified traveler',
     travel_type:'Travelers with pets',
     feedback:'Liked',
     date: new Date(),
@@ -10,8 +106,14 @@ var newdata =
     feedback_comment:'Cordial staff. Place is neat and clean.Wish cofffee is available 24 hours',
     spent_time:'Stayed 1 night in Feb 2021',
     like_counts:0
-    }
-    old.push(newdata);
+}
+       old.push(newdata);
+    
+
+
+
+
+
     localStorage.setItem('details', JSON.stringify(old));
     var arr = JSON.parse(localStorage.getItem('details'))
     Showreviews(arr)
